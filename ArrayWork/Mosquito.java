2fc8cdce89f32da9ee031778efb2f9ccb031c103
Andrew G Burkus <andrewburkus@mccallie.org>
2014-04-14 09:44:35 -0400

    Create Mosquito.java
    Mosquito file for lab 13.6

---
+++ ArrayWork/Mosquito.java
@@ -0,0 +1,22 @@
+public class Mosquito extends Critter {
+  private int lifespan;
+  boolean cowbitten;
+  
+  public Mosquito(Color color) {
+    super(color);
+    lifespan = 0;
+    cowbitten = false;
+  }
+  
+  public void processActors(ArrayList<Actors> actors) {
+    
+  }
+  
+  public Location selectMoveLocation(ArrayList<Locations> locs) {
+    
+  }
+  
+  public int DistanceBetweenLocations(Location loc1, Location loc2) {
+    return 
+  }
+}
