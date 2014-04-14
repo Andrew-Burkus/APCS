48e0ffbaaa04915b33485b74659af41ae78616a5
Andrew G Burkus <andrewburkus@mccallie.org>
2014-04-14 12:10:45 -0400

    Create MosquitoEgg.java

---
+++ ArrayWork/MosquitoEgg.java
@@ -0,0 +1,16 @@
+public class MosquitoEgg extends Actor {
+  private int lifespan;
+  
+  public MosquitoEgg() {
+    lifespan = 0;
+  }
+  
+  public void act() {
+    if(lifespan == 6) {
+      Mosquito mos = new Mosquito(Color.BLUE);
+      mos.putSelfInGrid(getGrid(), getLocation());
+      removeSelfFromGrid();
+    }
+    lifespan ++;
+  }
+}
