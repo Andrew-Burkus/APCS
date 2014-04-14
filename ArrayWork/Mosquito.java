98eec2368647bbf834bf0de29bb3a2743a840cc4
Andrew G Burkus <andrewburkus@mccallie.org>
2014-04-14 11:59:40 -0400

    Update Mosquito.java
    Finished

--- ArrayWork/Mosquito.java
+++ ArrayWork/Mosquito.java
@@ -10,13 +10,33 @@ public class Mosquito extends Critter {
   
   public void processActors(ArrayList<Actors> actors) {
     
+    lifespan ++;
   }
   
   public Location selectMoveLocation(ArrayList<Locations> locs) {
+    ArrayList<Location> bestlocs;
     
+    int minimum = DistanceBetweenLocations(this, goodloc);
+    int newdistance;
+    for(Location goodloc : locs) {
+      if((newdistance = DistanceBetweenLocations(this, goodloc)) >= minimum) {
+        minimum = newdistance;
+      }
+    }
+    for(Location goodloc : locs) {
+      if(goodloc.contains(minimum)) {
+        bestlocs.add(goodloc);
+      }
+    }
+    return super.selectMoveLocation(bestlocs);
   }
   
+   public void makeMove(Location loc) {
+        setDirection(getLocation().getDirectionToward(loc));
+        super.makeMove(loc);
+    }
+  
   public int DistanceBetweenLocations(Location loc1, Location loc2) {
-    return 
+    return distance(loc1, loc2);
   }
 }
